13f8a5b653fa71fed19a32b93902884edf6b0833
smajevicirfan <smajevic.irfan@gmail.com>
2018-10-18 10:17:56 +0200

    Problem Solved

--- src/ba/unsa/etf/Main.java
+++ src/ba/unsa/etf/Main.java
@@ -1,8 +1,29 @@
 package ba.unsa.etf;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static int sumaCifara(int n) {
+        int suma = 0;
+        while (n > 0) {
+            suma += n % 10;
+            n /= 10;
+        }
+
+        return suma;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Unesite broj n: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Brojevi izmedju 1 i n koji su djeljivi sa sumom svojih cifara su: ");
+        for (int i = 1; i < n; i++) {
+            if (i % sumaCifara(i) == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
